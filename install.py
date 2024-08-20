1bf7d29664fd52e0c02481867d329ddfe0d6fe25
SurnameGuy <felipe2020zen@protonmail.com>
2024-08-20 09:40:23 -0300

    a

---
+++ install.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import platform, os, stat, inspect, shutil
+from pathlib import Path
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+homepath = str(Path.home())
+
+# Caso seja retornado um erro, essa é a variável que você deve substituir por "username = o_seu_nome_de_usuário":
+username = os.getenv('SUDO_USER')
+# ---------------------------------------------------------------------------------------------------------------
+
+try:
+    if (len(username) < 1) or (username.lower() == "root"):
+        print(f'''ERRO CRÍTICO. O instalador foi encerrado inesperadamente para evitar possíveis problemas. Se você acha que isso é um engano, edite o script do instalador e substitua "username = os.getenv('SUDO_USER')" por "username = " e o seu nome de usuário. FAÇA ISSO POR SUA CONTA E RISCO!!!
+
+    Código do erro: INVALID_USERNAME_VARIABLE | Variable username = {username}''')
+        exit()
+except:
+    None
+
+def deletefile(filepath):
+    try:
+        return os.remove(filepath)
+    except:
+        return None
+
+def install():
+    if os.path.isfile("/usr/bin/nofapy") == True:
+        confirm = input("O Nofapy já está instalado no seu sistema. Deseja desinstalá-lo? (s/N): ")
+        if confirm.lower() == "s":
+            confirm = input("Deseja também remover os arquivos do Nofapy (i.e, seus dados)? (s/N): ")
+            if confirm.lower() == "s":
+                shutil.rmtree(f"/home/{username}/.config/nofapy")
+                print(f'· Deletado "/home/{username}/.config/nofapy" (shutil.rmtree ...)')
+                deletefile("/usr/bin/nofapy")
+                print(f'· Deletado "/usr/bin/nofapy" (os.remove ...)')
+                print(f'· Nofapy desinstalado sem manter os dados do usuário.')
+                exit()
+            else:
+                deletefile(f"/usr/bin/nofapy")
+                print(f'· Deletado "/usr/bin/nofapy" (os.remove ...)')
+                print(f'· Nofapy desinstalado mantendo os dados do usuário.')
+        else:
+            print("Cancelado.")
+            exit()
+    else:
+        confirm = input("Deseja instalar o Nofapy no seu sistema? (s/N): ")
+        if confirm.lower() == "s":
+            nofapypath = f"{path}/nofapy"
+            shutil.copyfile(nofapypath, "/usr/bin/nofapy")
+            print(f'· Copiado "{nofapypath}" para "/usr/bin/nofapy" (shutil.copyfile ...)')
+            os.chmod("/usr/bin/nofapy", 0o777)
+            print(f'· Definido "/usr/bin/nofapy" como executável (os.chmod 777)')
+            print(f'· Instalado! Para executar, apenas digite "nofapy" no seu terminal. Caso não funcionar, tente fechar e abrir novamente o seu terminal antes de executá-lo pela primeira vez.')
+        else:
+            print("Cancelado.")
+            exit()
+    
+if platform.system() == "Linux":
+    if not 'SUDO_UID' in os.environ:
+        print("ERRO: Este programa requer ser executado com privilégios de super-usuário!")
+        exit()
+    install()
+else:
+    confirm = input("Parece que você não está utilizando Linux. Este software não foi testado em outras plataformas, e não me responsabilizo por qualquer problema envolvendo seu funcionamento. Deseja continuar mesmo assim? (s/N): ")
+    if confirm.lower() == "s":
+        install()
+    else:
+        exit()
